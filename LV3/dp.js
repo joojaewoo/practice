5c521a620c3539430f948e38c8f6aeac1994bce1
Jae-Woo Joo <wnwodn4293@naver.com>
2020-12-07 21:24:17 +0900

    [feat] 프로그래머스 동적 프로그래밍 문제
    - 삼각형을 내려가며 더한 숫자의 최대값 구현
    - 양쪽 끝 반복문 한번으로 구한 후 내려가면서 안에 내용 채움

---
+++ LV3/dp.js
@@ -0,0 +1,21 @@
+const solution = (triangle) => {
+  var answer = 0;
+  const dp = [];
+  for (let i = 0; i < triangle.length; i++) {
+    dp[i] = [];
+  }
+  dp[0][0] = triangle[0][0];
+  for (let i = 1; i < triangle.length; i++) {
+    dp[i][i] = triangle[i][i] + dp[i - 1][i - 1];
+    dp[i][0] = triangle[i][0] + dp[i - 1][0];
+  }
+  for (let i = 2; i < triangle.length; i++) {
+    for (let j = 1; j < i; j++) {
+      dp[i][j] = Math.max(dp[i - 1][j - 1], dp[i - 1][j]) + triangle[i][j];
+    }
+  }
+  dp[dp.length - 1].sort((a, b) => b - a);
+  return dp[dp.length - 1][0];
+};
+
+console.log(solution([[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]]));
