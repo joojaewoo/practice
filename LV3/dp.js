0fe4180ebbcf2bd0d2c7a50fa4d2eda1eeaee5ef
Jae-Woo Joo <wnwodn4293@naver.com>
2020-12-08 12:55:30 +0900

    [refactor] 필요없는 변수 삭제
    - dp를 반환하므로 answer 이 필요없어 삭제

--- LV3/dp.js
+++ LV3/dp.js
@@ -1,13 +1,13 @@
 const solution = (triangle) => {
-  var answer = 0;
   const dp = [];
   for (let i = 0; i < triangle.length; i++) {
     dp[i] = [];
   }
   dp[0][0] = triangle[0][0];
   for (let i = 1; i < triangle.length; i++) {
-    dp[i][i] = triangle[i][i] + dp[i - 1][i - 1];
+    p[i][i] = triangle[i][i] + dp[i - 1][i - 1];
     dp[i][0] = triangle[i][0] + dp[i - 1][0];
+    d;
   }
   for (let i = 2; i < triangle.length; i++) {
     for (let j = 1; j < i; j++) {
