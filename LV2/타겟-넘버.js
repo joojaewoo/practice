e7f5b270ae7ea8fb61182454668c79dda23393b6
Jae-Woo Joo <wnwodn4293@naver.com>
2021-01-18 22:26:30 +0900

    [feat] 타겟 넘버
    - 함수 내부에 재귀함수 구현

--- LV2/타겟-넘버.js
+++ LV2/타겟-넘버.js
@@ -1,12 +1,12 @@
 const solution = (numbers, target) => {
+  const dfs = (numbers, k, target, sum) => {
+    let ans = 0;
+    if (numbers.length === k) return sum === target ? 1 : 0;
+    ans += dfs(numbers, k + 1, target, sum + numbers[k]);
+    ans += dfs(numbers, k + 1, target, sum - numbers[k]);
+    return ans;
+  };
   return dfs(numbers, 0, target, 0);
 };
 
-const dfs = (numbers, k, target, sum) => {
-  let ans = 0;
-  if (numbers.length === k) return sum === target ? 1 : 0;
-  ans += dfs(numbers, k + 1, target, sum + numbers[k]);
-  ans += dfs(numbers, k + 1, target, sum - numbers[k]);
-  return ans;
-};
 console.log(solution([1, 1, 1, 1, 1], 3));
