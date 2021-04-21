72559b86276cd063ee058cae8c9833c1eb43e18f
Jae-Woo Joo <wnwodn4293@naver.com>
2021-04-21 17:12:01 +0900

    [LV3] 야근지수
    - 남은 일의 합이 근무시간 보다 작을때 0을 반환
    - obj의 key값으로 시간 value값으로 시간의 개수를 저장 arr에 obj의 key값을 내림차순으로 저장
    - arr을 사용해 제일 높은 key값의 시간을 하나씩 내려주면서 계산

---
+++ LV3/야근지수.js
@@ -0,0 +1,36 @@
+const solution = (n, works) => {
+  const N = works.length;
+  const sum = works.reduce((acc, cur) => acc + cur, 0);
+  if (sum <= n) return 0;
+  const obj = {};
+  const arr = works.filter((item) => {
+    if (!obj[item]) {
+      obj[item] = 1;
+      return item;
+    } else obj[item]++;
+  });
+  arr.sort((a, b) => b - a);
+  while (n > 0) {
+    const [idx] = arr;
+    const value = obj[idx];
+    if (value <= n) {
+      delete obj[idx];
+      if (arr[1] !== idx - 1) {
+        arr[0] = idx - 1;
+        obj[idx - 1] = 0;
+      } else arr.shift();
+      obj[arr[0]] += value;
+    } else {
+      obj[idx] -= n;
+      if (!obj[idx - 1]) obj[idx - 1] = 0;
+      obj[idx - 1] += n;
+    }
+    n -= value;
+  }
+  let ans = 0;
+  for (let idx in obj) {
+    ans += idx * idx * obj[idx];
+  }
+  return ans;
+};
+console.log(solution(3, [1, 1]));
