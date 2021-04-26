d922328a19dd7ef610a0a915b13a476df3b16763
Jae-Woo Joo <wnwodn4293@naver.com>
2021-04-26 17:38:32 +0900

    [LV2] 조이스틱
    - 다음번째 조절할 위치와 움직이는 카운트를 반환하는 find 함수 구현
    - 위치에서 가장 작은 움직임으로 원하는 위치 설정
    - 움직이는 카운트를 ans에 더해주면서 구함

--- LV2/조이-스틱.js
+++ LV2/조이-스틱.js
@@ -1,21 +1,44 @@
 const solution = (name) => {
-  // const find = (visit, idx, names) => {
-  //   let r = idx+1;
-  //   let l = idx -1;
-  //   for(let i =0;i<names.length;i++){
-  //     if(visit[r])
-  //   }
-  // }
+  const find = (visit, idx, names) => {
+    if (!visit[idx] && names[idx] !== 'A') {
+      visit[idx] = true;
+      return [idx, 0];
+    }
+    let cnt = 1;
+    let l = idx - 1 < 0 ? visit.length - 1 : idx - 1;
+    let r = idx + 1 >= visit.length ? 0 : idx + 1;
+    while (r !== l) {
+      if (!visit[r] && names[r] !== 'A') {
+        visit[r] = true;
+        return [r, cnt];
+      }
+      if (!visit[l] && names[l] !== 'A') {
+        visit[l] = true;
+        return [l, cnt];
+      }
+      l = l - 1 < 0 ? visit.length - 1 : l - 1;
+      r = r + 1 >= visit.length ? 0 : r + 1;
+      cnt++;
+    }
+    visit[r] = true;
+    return [r, cnt];
+  };
   let ans = 0;
   let pivot = 'A';
   const names = name.split('');
   const visit = names.map((item) => (item === 'A' ? true : false));
-  for (let char of names) {
-    const diff = pivot.charCodeAt(0) - 'A'.charCodeAt(0) + 'Z'.charCodeAt(0) - char.charCodeAt(0);
-    if (diff <= Math.abs(pivot.charCodeAt(0) - char.charCodeAt(0))) ans += diff + 1;
-    else ans += Math.abs(pivot.charCodeAt(0) - char.charCodeAt(0)) + 1;
-    ans++;
-    pivot = char;
+  let num = visit.reduce((acc, cur) => acc + (cur ? 0 : 1), 0);
+  let idx = 0;
+  while (num > 0) {
+    const [index, move] = find(visit, idx, names);
+    const char = names[index];
+    const diff = 'Z'.charCodeAt(0) - char.charCodeAt(0) + 1;
+    if (diff <= Math.abs(pivot.charCodeAt(0) - char.charCodeAt(0))) ans += diff;
+    else ans += Math.abs(pivot.charCodeAt(0) - char.charCodeAt(0));
+    ans += move;
+    idx = index;
+    num--;
   }
-  return ans - 1;
+  return ans;
 };
+console.log(solution('JEROEN'));
