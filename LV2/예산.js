7feeef9205c7c68f347cd9d03485d589aff4f543
Jae-Woo Joo <wnwodn4293@naver.com>
2021-05-09 20:03:55 +0900

    [LV2] 예산
    - 각 부서별 신청금액을 오름차순으로 정렬
    - 각 값을 더해가며 총 예산보다 작을때 까지 진행
    - 현재까지 충족시킨 부서의 개수를 반환

---
+++ LV2/예산.js
@@ -0,0 +1,9 @@
+const solution = (d, budget) => {
+  d.sort((a, b) => a - b);
+  let sum = 0;
+  for (let i = 0; i < d.length; i++) {
+    if (sum + d[i] <= budget) sum += d[i];
+    else return i;
+  }
+  return d.length;
+};
