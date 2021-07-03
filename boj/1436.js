c526f77e8fcd4c180a6ddffdc64b0ca9d9de4b4c
Jae-Woo Joo <wnwodn4293@naver.com>
2021-07-03 14:30:01 +0900

    [BOJ] 1436
    - 영화감독 숌 코드 수정
    - 시간초과로 인해서 코드 수정

--- boj/1436.js
+++ boj/1436.js
@@ -1,22 +1,14 @@
 const fs = require('fs');
-const input = fs.readFileSync('dev/stdin', 'utf8').trim().split('\n');
+const input = fs.readFileSync('./stdin', 'utf8').trim().split('\n');
 
 const solution = (N) => {
-  let i = 0;
-  let cnt = 0;
-  while (++i) {
-    let temp = i;
-    let flag = 0;
-    while (temp) {
-      if (temp % 1000 === 666) flag = 1;
-      temp /= 10;
-    }
-    if (flag) {
-      cnt++;
-      if (cnt === N) break;
-    }
+  let num = 666;
+  let count = 1;
+  while (count !== N) {
+    num++;
+    if (String(num).includes('666')) count++;
   }
-  return i;
+  return num;
 };
 
 console.log(solution(+input[0]));
