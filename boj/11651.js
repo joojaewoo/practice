55ae2ebbe4d514e0ee7aa400729f17cfc3ecbb02
Jae-Woo Joo <wnwodn4293@naver.com>
2021-07-16 20:21:15 +0900

    [BOJ] 11651
    - y좌표 오름차순으로 정렬하기

---
+++ boj/11651.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const [_, ...input] = fs.readFileSync('dev/stdin').toString().trim().split('\n');
+
+const solution = (input) =>
+  input
+    .map((item) => item.split(' '))
+    .sort(([x1, y1], [x2, y2]) => (y1 === y2 ? x1 - x2 : y1 - y2))
+    .map(([x, y]) => `${x} ${y}`)
+    .join('\n');
+
+console.log(solution(input));
